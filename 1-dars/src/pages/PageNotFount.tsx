fa954c9d8e4ac47043607815243cd5d6943c12cf
Coder <rajabovazizbek627@gmail.com>
2025-04-12 12:10:24 +0500

    Bu birinchi loyihani kodi

--- 1-dars/src/pages/PageNotFount.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PageNotFount() {
-  return (
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh"}}>
-      <h1 style={{color:"red",fontSize:100}}>404</h1>
-    </div>
-  )
-}
